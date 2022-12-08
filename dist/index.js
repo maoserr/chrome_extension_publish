5a5313be9bbc032c0c090756826dd839b028aeab
maoserr <maoserr@gmail.com>
2022-12-08 13:46:49 -0600

    updating eol

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-import './sourcemap-register.cjs';import { createRequire as __WEBPACK_EXTERNAL_createRequire } from "module";
+import { createRequire as __WEBPACK_EXTERNAL_createRequire } from "module";
 /******/ var __webpack_modules__ = ({
 
 /***/ 7351:
@@ -16644,5 +16644,3 @@ run().then();
 
 })();
 
-
-//# sourceMappingURL=index.js.map
